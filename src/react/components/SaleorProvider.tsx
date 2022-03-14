0e402c47f87fada7be3353c8328200c5789ab70f
mathur619 <prakharmathur619@gmail.com>
2022-03-14 12:16:51 +0530

    reset saleorclient to previous

--- src/react/components/SaleorProvider.tsx
+++ src/react/components/SaleorProvider.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { SaleorClient } from "../../core";
-import { ApolloClient, ApolloProvider } from "@apollo/client";
 
 export type SaleorContextType = {
   client: SaleorClient;
@@ -8,10 +7,10 @@ export type SaleorContextType = {
 
 export const SaleorContext = React.createContext<SaleorClient | null>(null);
 
-export const SaleorProvider: React.FC<{
-  client: SaleorClient;
-  apolloClient: ApolloClient<any>;
-}> = ({ client, apolloClient, children }) => {
+export const SaleorProvider: React.FC<{ client: SaleorClient }> = ({
+  client,
+  children,
+}) => {
   const [context, setContext] = React.useState<SaleorClient>(client);
 
   React.useEffect(() => {
@@ -21,7 +20,7 @@ export const SaleorProvider: React.FC<{
   if (context) {
     return (
       <SaleorContext.Provider value={context}>
-        <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
+        {children}
       </SaleorContext.Provider>
     );
   }
