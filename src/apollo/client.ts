3bfe881f5df1907e150f85074cf8b0cbff16f762
Hitesh Datt <hdk.hitesh26@gmail.com>
2022-06-08 13:23:09 +0530

    types

--- src/apollo/client.ts
+++ src/apollo/client.ts
@@ -373,13 +373,13 @@ export const createApolloClient = (
   const authLink = setContext(async (_, { headers }) => {
     let ip, fbp, fbq;
     if (typeof window !== "undefined") {
-      function getCookie(name) {
+      function getCookie(name:string) {
         // Split cookie string and get all individual name=value pairs in an array
         var cookieArr = document.cookie.split(";");
 
         // Loop through the array elements
         for (var i = 0; i < cookieArr.length; i++) {
-          var cookiePair = cookieArr[i].split("=");
+          var cookiePair:string = cookieArr[i].split("=");
 
           /* Removing whitespace at the beginning of the cookie name
           and compare it with the given string */
