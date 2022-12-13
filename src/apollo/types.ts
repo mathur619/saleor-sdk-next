bceca925ef78011ccfeaa49aeec319196683bc2a
HiteshDatt <hdk.hitesh26@gmail.com>
2022-12-13 13:28:23 +0530

    corrected types for wishlist

--- src/apollo/types.ts
+++ src/apollo/types.ts
@@ -18659,8 +18659,8 @@ export type CreateCheckoutNextMutation = {
 };
 
 export type UpdateCheckoutShippingMethodNextMutationVariables = Exact<{
-  checkoutId: Scalars["ID"];
-  shippingMethodId: Scalars["ID"];
+  checkoutId?: Scalars["ID"];
+  shippingMethodId?: Scalars["ID"];
 }>;
 
 export type UpdateCheckoutShippingMethodNextMutation = {
