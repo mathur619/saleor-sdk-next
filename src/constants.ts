f453220f9803bb118afc563c09516cd977e71b1a
Koustav <koustavlmcr17@gmail.com>
2023-06-28 13:50:03 +0530

    Added updateItemWithLinesRest

--- src/constants.ts
+++ src/constants.ts
@@ -12,3 +12,76 @@ export const REST_API_ENDPOINTS = {
   UPDATE_CART: "update_cart/",
   CREATE_CHECKOUT: "create_checkout/",
 };
+
+export const dummyCheckoutFields = {
+  availablePaymentGateways: [
+    {
+      currencies: ["INR"],
+      id: "mirumee.payments.razorpay",
+      name: "Razorpay",
+      __typename: "PaymentGateway",
+      config: [],
+    },
+  ],
+  billingAddress: null,
+  cashback: {
+    amount: "0.00",
+    willAddOn: null,
+    __typename: "CashbackType",
+  },
+  discount: {
+    currency: "INR",
+    amount: 0,
+    __typename: "Money",
+  },
+  email: "dummy@dummy.com",
+  paymentMethod: {
+    cashbackDiscountAmount: 0,
+    couponDiscount: "0",
+    prepaidDiscountAmount: 0,
+    __typename: "PaymentMethodType",
+  },
+  shippingAddress: null,
+  shippingMethod: null,
+  shippingPrice: {
+    gross: {
+      amount: 0,
+      currency: "INR",
+      __typename: "Money",
+    },
+    net: {
+      amount: 0,
+      currency: "INR",
+      __typename: "Money",
+    },
+    __typename: "TaxedMoney",
+  },
+  subtotalPrice: {
+    gross: {
+      amount: 0,
+      currency: "INR",
+      __typename: "Money",
+    },
+    net: {
+      amount: 0,
+      currency: "INR",
+      __typename: "Money",
+    },
+    __typename: "TaxedMoney",
+  },
+  totalPrice: {
+    gross: {
+      amount: 0,
+      currency: "INR",
+      __typename: "Money",
+    },
+    net: {
+      amount: 0,
+      currency: "INR",
+      __typename: "Money",
+    },
+    __typename: "TaxedMoney",
+  },
+  translatedDiscountName: null,
+  voucherCode: null,
+};
