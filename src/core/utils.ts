293c099010e63826cd859d7e227d917c548f7890
Koustav <koustavlmcr17@gmail.com>
2023-09-20 12:36:36 +0530

    added useUtilFunctions hook

---
+++ src/core/utils.ts
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { axiosRequest } from "../apollo";
+import { REST_API_METHODS_TYPES, WIZZY_ENDPOINTS } from "../constants";
+
+export interface UtilityFunctionsSDK {
+  searchProducts: (queryOptions: any) => any;
+}
+
+export const utilityFunctions = ({
+  apolloClient: client,
+  restApiUrl,
+  wizzyConfig,
+}: any): UtilityFunctionsSDK => {
+  const searchProducts = async (queryOptions = null) => {
+    if (queryOptions && wizzyConfig.headers) {
+      try {
+        const wizzyHeaders = wizzyConfig.headers;
+        const response = await axios({
+          url: WIZZY_ENDPOINTS.SEARCH,
+          method: REST_API_METHODS_TYPES.POST,
+          data: queryOptions,
+          headers: { ...wizzyHeaders },
+        });
+
+        return response;
+      } catch (error) {
+        console.log("Error occurred in axiosRequest", error);
+        return;
+      }
+    }
+    return null;
+  };
+  return {
+    searchProducts,
+  };
+};
